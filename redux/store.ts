873e3247c8cd2502245f7ecef269efa7a30517d7
Ruchira Ranasinghe <rhvruchira@gmail.com>
2025-06-15 23:01:21 +0530

    feat: add customer slice to store configuration

--- redux/store.ts
+++ redux/store.ts
@@ -2,18 +2,18 @@ import { configureStore, UnknownAction } from "@reduxjs/toolkit"
 import authSlice from "@/redux/features/authSlice";
 import supplierSlice from "@/redux/features/supplierSlice"
 import productSlice from "./features/productSlice";
+import customerSlice from "./features/customerSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authSlice,
     supplier: supplierSlice,
     product: productSlice,
+    customer: customerSlice,
   },
 })
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
 export type ProductSliceState = ReturnType<typeof productSlice>
-
-
-
+export type CustomerSliceState = ReturnType<typeof customerSlice>;
